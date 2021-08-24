2e02dbac13da160c95cfced957ecec9a263c1a41
Hannah Malm <hannahemalm616@gmail.com>
2021-08-24 18:59:27 -0400

    commit tree ef76ecd27e2dbf42edd939a639bab473585b944f

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+require './config/environment'
+
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
@@ -5,5 +7,42 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
+  #Controller action that will render a form to create a new recipe - create and save new recipe
+ 
 
+  post "/recipes" do 
+    @recipe = Recipe.create(params)
+    redirect to "/recipes/#{@recipe.id}"
+  end 
+
+  #controller action that uses RESTful routes to display a single recipe
+  get "/recipes/:id" do 
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show 
+  end 
+  #controller action that uses RESTful routes and renders a form to edit SINGLE recipe - update entry in db, redirect to show page
+  get "/recipes/:id/edit" do 
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit 
+  end 
+
+  patch "/recipes/:id" do 
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save 
+    redirect to "/recipes/#{@recipe.id}"
+  end 
+  #controller action - index - that displays all the recipes in the database 
+  get "/recipes" do 
+    @recipes = Recipe.all 
+    erb :index 
+  end 
+  #add recipe show page a form for deletion - submit a ctonroller action, and redirect to index page
+  delete "/recipes/:id" do 
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.delete
+    redirect to "/recipes"
+  end
 end
